e8c6802b6a4153c40f92151182f829fd33e14cf4
JFMajcher <majcherjakub95@gmail.com>
2026-03-17 14:28:38 +0100

    Added different ways to output messages

--- profit_calculator.go
+++ profit_calculator.go
@@ -19,11 +19,25 @@ func main() {
 	profit = EBT * (1 - taxRate/100)
 	ratio = profit / EBT
 
-	fmt.Print("Your earnings before taxes (EBT) are equal to: ")
-	fmt.Println(EBT)
-	fmt.Print("Your profit after taxes is equal to: ")
-	fmt.Println(profit)
-	fmt.Print("Profit to EBT ratio: ")
-	fmt.Println(ratio)
+	fmt.Println("Your earnings before taxes (EBT) are equal to: ", EBT)
+	fmt.Println("Your profit after taxes is equal to: ", profit)
+	fmt.Println("Profit to EBT ratio: ", ratio)
+
+	//another way to print values
+	fmt.Printf("\nEBT: %v\nProfit: %.2f\nProfit to EBT ratio: %.2f\n\n", EBT, profit, ratio)
+
+	//with formatting before printing:
+	formattedEbtMsg := fmt.Sprintf("Earnings before taxes (EBT): %.2f", EBT)
+	formattedProfitMsg := fmt.Sprintf("Profit after taxes: %.2f\n", profit)
+	formattedRatioMsg := fmt.Sprintf("EBT to profit ratio: %.6f\n", EBT/profit)
+
+	fmt.Println(formattedEbtMsg)
+	fmt.Print(formattedProfitMsg, formattedRatioMsg)
+
+	//using `` you can print the strings with the linebreaks and tabulators
+	fmt.Printf(`
+	Your earnings before taxes (EBT) are equal to: %v
+	Profit: %.2f
+	Profit to EBT ratio: %.2f\n\n`, EBT, profit, ratio)
 
 }
